0646f976cb4391f500aa8ec50f3cdb79ea094173
Maria Ivanova <44948788+mimivanova93@users.noreply.github.com>
2019-01-27 12:25:40 +0200

    project

--- Project-19.01.27/project1.php
+++ Project-19.01.27/project1.php
@@ -1,29 +1,29 @@
 <?php
 
-// $string_array=['abc','def','e', 'e', 'g', 'e', 'ghi'];
-$string_array=['abc','def','ghi'];
+$string_array=['abc','def','e', 'e', 'g', 'e', 'ghi'];
+// $string_array=['abc','def','ghi'];
 // $string_array=['abc','def','ssfv','segtbs','ss','ghi'];
 $count=count($string_array);
 
 $f_el = $string_array[0];
-var_dump($f_el);
+// var_dump($f_el);
 $len = strlen($f_el);
-var_dump($len);
+// var_dump($len);
 $elem = [$f_el[$len-1]];
 var_dump($elem);
 
 for($i=1;$i<$count-1;$i++){
 	$el_count = count($elem);
-	var_dump($el_count);
+	// var_dump($el_count);
 	$s_len = strlen($string_array[$i]);
-	var_dump($s_len);
+	// var_dump($s_len);
 	if ($s_len == 1) {
 		$elem[$el_count-1] = $elem[$el_count-1].$string_array[$i][0];
-		var_dump($elem);
+		// var_dump($elem);
 	} else {
 		$elem[$el_count-1] = $elem[$el_count-1].$string_array[$i][0];
 		$elem[$el_count] = $string_array[$i][$s_len-1];
-		var_dump($elem);
+		// var_dump($elem);
 	}
 }
 
